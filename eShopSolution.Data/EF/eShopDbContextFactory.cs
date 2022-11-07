53d1d8d71f59554f4707239b6bf4aa4f5f644995
Hairicorn99 <mdhaic5tantrao2018@gmail.com>
2022-11-07 15:21:30 +0700

    create database structure

---
+++ eShopSolution.Data/EF/eShopDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class eShopDbContextFactory : IDesignTimeDbContextFactory<eShopDbContext>
+    {
+        public eShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connnectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionBuilder = new DbContextOptionsBuilder<eShopDbContext>();
+            optionBuilder.UseSqlServer(connnectionString);
+
+            return new eShopDbContext(optionBuilder.Options);
+        }
+    }
+}
